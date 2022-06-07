47730166eb1ad39aacbd0bef70960c585e88690e
aiboforcen <aiboforcen8@yandex.ru>
2022-06-07 17:28:56 +0300

    server_and_client_templates

---
+++ server/config/config.js
@@ -0,0 +1,19 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+
+const corsConfig = require('./corsConfig')
+
+const config = (app) => {
+  app.disable("x-powered-by");
+  app.use(express.json());
+  app.use(
+    express.urlencoded({
+      extended: true,
+    })
+  );
+  app.use(cors());
+  app.use(cors(corsConfig));
+};
+
+module.exports = config;
